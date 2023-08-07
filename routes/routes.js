5ce0f9cf3fada829701fa44df82770d43dd94125
Andrei <andrei.ribeirow@gmail.com>
2023-08-07 22:53:16 +1000

    cutting routes from the server.js and pasting it to the routes.js and exporting the module. Also importing the routes module in the server.js

---
+++ routes/routes.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const { createNewNote, deleteNote } = require('./helpers/helpers');
+
+function setupRoutes(app) {
+    // GET request to '/api/notes' returns all not
+    app.get('/api/notes', (req, res) => {
+        res.json(app.locals.allNotes);
+    });
+
+    // GET request to the root path returns the 'index.html' file
+    app.get('/', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'));
+    });
+
+    // GET request to '/notes' returns the 'notes.html' file
+    app.get('/notes', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/notes.html'));
+    });
+
+    // For any other undefined routes, return the 'index.html' file
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, './public/index.html'));
+    });
+
+    // POST request to '/api/notes' creates a new note
+    app.post('/api/notes', async (req, res) => {
+        try {
+            const newNote = await createNewNote(req.body, app.locals.allNotes);
+            res.json(newNote);
+        } catch (err) {
+            res.status(500).json({ error: 'Failed to create note.' });
+        }
+    });
+
+    // DELETE request to '/api/notes/:id' deletes a note with the specified ID
+    app.delete('/api/notes/:id', async (req, res) => {
+        try {
+            const noteIdToDelete = req.params.id;
+            await deleteNote(noteIdToDelete, app.locals.allNotes);
+            app.locals.allNotes = app.locals.allNotes.filter((note) => note.id !== noteIdToDelete);
+            res.json(true);
+        } catch (err) {
+            res.status(500).json({ error: 'Failed to delete note.' });
+        }
+    });
+}
+
+module.exports = { setupRoutes };
--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 // Set the server's port number to either the environment variable PORT or 3000
 const PORT = process.env.PORT || 3000;
 
+// Define the path to the JSON file where notes will be stored
+const DB_FILE_PATH = path.join(__dirname, './db/db.json');
+
 // Import required modules
-const path = require('path');
 const express = require('express');
+const path = require('path');
 const { v4: uuidv4 } = require('uuid');
+const fs = require('fs').promises;
 
 // Create an Express application
 const app = express();
 
 // Import modules
 const { readDataFromFile, createNewNote, deleteNote } = require('./helpers/helpers');
+const { setupRoutes } = require('./routes/routes');
 
-// Define the path to the JSON file where notes will be stored
-const DB_FILE_PATH = path.join(__dirname, './db/db.json');
 
 // Middlewares:
-// - Parse incoming JSON data
-// - Parse incoming URL-encoded data
-// - Serve static files from the 'public' directory
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
@@ -38,50 +38,7 @@ async function initialize() {
     }
 }
 
-// Set up the routes
-
-// GET request to '/api/notes' returns all not
-app.get('/api/notes', (req, res) => {
-    res.json(app.locals.allNotes);
-});
-
-// GET request to the root path returns the 'index.html' file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-// GET request to '/notes' returns the 'notes.html' file
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/notes.html'));
-});
-
-
-// For any other undefined routes, return the 'index.html' file
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-// POST request to '/api/notes' creates a new note
-app.post('/api/notes', async (req, res) => {
-    try {
-        const newNote = await createNewNote(req.body, app.locals.allNotes);
-        res.json(newNote);
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to create note.' });
-    }
-});
-
-// DELETE request to '/api/notes/:id' deletes a note with the specified ID
-app.delete('/api/notes/:id', async (req, res) => {
-    try {
-        const noteIdToDelete = req.params.id;
-        await deleteNote(noteIdToDelete, app.locals.allNotes);
-        app.locals.allNotes = app.locals.allNotes.filter((note) => note.id !== noteIdToDelete);
-        res.json(true);
-    } catch (err) {
-        res.status(500).json({ error: 'Failed to delete note.' });
-    }
-});
+setupRoutes(app);
 
 // Call the initialize function to start the server
 initialize();
